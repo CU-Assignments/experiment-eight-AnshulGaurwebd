c14a9881f970199dbe9e3edafb2bbea741ee961c
AnshulGaurwebd <159431971+AnshulGaurwebd@users.noreply.github.com>
2025-04-06 21:06:23 +0530

    Create Easy2.java

---
+++ Easy2.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+  
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        if ("admin".equals(username) && "password123".equals(password)) {
+            out.println("<html><body>");
+            out.println("<h2>Welcome, " + username + "!</h2>");
+            out.println("</body></html>");
+        } else {
+            out.println("<html><body>");
+            out.println("<h2>Login Failed. Invalid username or password.</h2>");
+            out.println("<a href='login.html'>Try Again</a>");
+            out.println("</body></html>");
+        }
+    }
+}
